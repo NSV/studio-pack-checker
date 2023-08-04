d916921c435bcad0019c805be1f7f1d59357a8ff
NSV <1151965+NSV@users.noreply.github.com>
2023-08-04 23:10:24 +0200

    Use by default ffmpeg
    Signed-off-by: NSV <1151965+NSV@users.noreply.github.com>

--- src/main/java/org/studio/checker/processor/AudioProcessor.java
+++ src/main/java/org/studio/checker/processor/AudioProcessor.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
 import org.studio.checker.FFmpegCreator;
 import studio.core.v1.model.asset.MediaAsset;
 import studio.core.v1.model.asset.MediaAssetType;
@@ -16,6 +17,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.function.Function;
 
@@ -38,59 +40,70 @@ public class AudioProcessor implements Function<MediaAsset, byte[]> {
     @SneakyThrows
     public byte[] apply(MediaAsset mediaAsset) {
         byte[] rawData = mediaAsset.getRawData();
-        if (MediaAssetType.MP3 != mediaAsset.getType()) {
-            if (compress) {
-                log.debug("Converting audio asset into MP3");
-                rawData = AudioConversion.anyToMp3(rawData);
-            }
-        } else {
-            // Check that the file is MONO / 44100Hz
-            try (ByteArrayInputStream bais = new ByteArrayInputStream(rawData)) {
-                AudioFormat audioFormat = AudioSystem.getAudioFileFormat(bais).getFormat();
+        try {
+            if (MediaAssetType.MP3 != mediaAsset.getType()) {
                 if (compress) {
-                    if (audioFormat.getChannels() != AudioConversion.CHANNELS
-                            || audioFormat.getSampleRate() != AudioConversion.MP3_SAMPLE_RATE) {
-                        log.debug("Re-encoding MP3 audio asset");
-                        rawData = AudioConversion.anyToMp3(rawData);
-                    }
-                }
-            } catch (UnsupportedAudioFileException e) {
-                if (repair) {
-                    log.debug("Audio format not supported by javax.sound, use FFmpeg as failback");
-                    // Need to flush the data in a file in order to provide it to ffmpeg
-                    File inputTempFile = File.createTempFile("spc_audio_inputTempFile", ".audio");
-                    try (FileOutputStream fos = new FileOutputStream(inputTempFile)) {
-                        fos.write(rawData);
-                        fos.flush();
-
-                        File outputTempFile = File.createTempFile("spc_audio_outputTempFile", MediaAssetType.MP3.firstExtension());
-                        FFmpegBuilder builder = new FFmpegBuilder()
-                                .setInput(inputTempFile.getAbsolutePath())
-                                .addOutput(outputTempFile.getAbsolutePath())
-                                .setFormat(MediaAssetType.MP3.name())
-                                .setAudioChannels(AudioConversion.CHANNELS)
-                                .setAudioCodec(MediaAssetType.MP3.name().toLowerCase())
-                                .setAudioSampleRate(Math.round(AudioConversion.MP3_SAMPLE_RATE))
-                                .setAudioBitRate(AudioConversion.MP3_BITSIZE * 1024)
-                                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
-                                .done();
-
-                        FFmpegCreator.getFFmpegExecutor().createJob(builder).run();
-
-                        rawData = Files.readAllBytes(outputTempFile.toPath());
-                    }
-                } else {
-                    throw e;
-                }
-            } finally {
-                if (compress) {
+                    log.debug("Converting audio asset into MP3");
+                    rawData = anyToMp3(rawData);
+                    
                     // Remove potential ID3 tags
                     rawData = ID3Tags.removeID3v1Tag(rawData);
                     rawData = ID3Tags.removeID3v2Tag(rawData);
                 }
+            } else {
+                // Check that the file is MONO / 44100Hz
+                try (ByteArrayInputStream bais = new ByteArrayInputStream(rawData)) {
+                    AudioFormat audioFormat = AudioSystem.getAudioFileFormat(bais).getFormat();
+                    if (compress) {
+                        if (audioFormat.getChannels() != AudioConversion.CHANNELS
+                                || audioFormat.getSampleRate() != AudioConversion.MP3_SAMPLE_RATE) {
+                            log.debug("Re-encoding MP3 audio asset");
+                            rawData = anyToMp3(rawData);
+                        }
+                    }
+                } finally {
+                    if (compress) {
+                        // Remove potential ID3 tags
+                        rawData = ID3Tags.removeID3v1Tag(rawData);
+                        rawData = ID3Tags.removeID3v2Tag(rawData);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            if (repair) {
+                log.debug("Audio format not supported by javax.sound, use FFmpeg as failback");
+                // Need to flush the data in a file in order to provide it to ffmpeg
+                rawData = anyToMp3(rawData);
+            } else {
+                throw e;
             }
         }
         return rawData;
+    }
+
+    private static byte[] anyToMp3(byte[] rawData) throws IOException, UnsupportedAudioFileException {
+        File inputTempFile = File.createTempFile("spc_audio_inputTempFile", ".audio");
+        try (FileOutputStream fos = new FileOutputStream(inputTempFile)) {
+            fos.write(rawData);
+            fos.flush();
+
+            File outputTempFile = File.createTempFile("spc_audio_outputTempFile", MediaAssetType.MP3.firstExtension());
+            FFmpegBuilder builder = new FFmpegBuilder()
+                    .setInput(inputTempFile.getAbsolutePath())
+                    .addOutput(outputTempFile.getAbsolutePath())
+                    .setAudioChannels(AudioConversion.CHANNELS)
+                    .setAudioSampleRate(Math.round(AudioConversion.MP3_SAMPLE_RATE))
+                    .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
+                    .done();
+
+            FFmpegJob job = FFmpegCreator.getFFmpegExecutor().createJob(builder);
+            job.run();
+            if (FFmpegJob.State.FINISHED.equals(job.getState())) {
+                return Files.readAllBytes(outputTempFile.toPath());
+            } else {
+                return AudioConversion.anyToMp3(rawData);
+            }
+        }
     }
 
 }
